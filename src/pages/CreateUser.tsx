0abafa8ca4c6d6e3bd63e8e36c36930e118a0cde
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-24 20:20:51 -0600

    add page to display the user creation form

---
+++ src/pages/CreateUser.tsx
@@ -0,0 +1,13 @@
+import UserForm from "../components/userForm";
+
+const CreateUser = () => {
+  return (
+    <div className="container">
+      <div className="container__form">
+        <UserForm />
+      </div>
+    </div>
+  );
+};
+
+export default CreateUser;
