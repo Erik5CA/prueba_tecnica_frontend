f49da1feddab9d2907b937d4ca82c5541a0e1838
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-24 20:21:56 -0600

    add page to display the user data update form

---
+++ src/pages/EditUser.tsx
@@ -0,0 +1,15 @@
+import UserForm from "../components/userForm";
+import { useParams } from "react-router-dom";
+
+const EditUser = () => {
+  const { id } = useParams();
+  return (
+    <div className="container">
+      <div className="container__form">
+        <UserForm isCreate={false} id={id} />
+      </div>
+    </div>
+  );
+};
+
+export default EditUser;
