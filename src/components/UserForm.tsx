d192d6908922822cbfade4d1d19616fb16cefacf
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-24 20:16:20 -0600

    add UserForm component to manage the creation and update of users

---
+++ src/components/UserForm.tsx
@@ -0,0 +1,151 @@
+import { useRef, useState } from "react";
+import { User } from "../types";
+import { userService } from "../services/userService";
+import { LoaderCircle } from "lucide-react";
+import "./UserForm.css";
+
+const UserForm = ({
+  isCreate = true,
+  id,
+}: {
+  isCreate?: boolean;
+  id?: string;
+}) => {
+  const [user, setUser] = useState<User>({
+    name: "",
+    lastname: "",
+    email: "",
+    image: null,
+    password: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const imageRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUser((prev) => ({ ...prev, image: e.target.files![0] }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const formData = new FormData();
+    formData.append("image", user.image || "");
+    formData.append("password", user.password);
+    formData.append("name", user.name);
+    formData.append("lastname", user.lastname);
+    formData.append("email", user.email);
+    try {
+      if (isCreate) {
+        await userService.createUser(formData);
+      } else {
+        await userService.updateUser(id!, formData);
+      }
+      setUser({
+        name: "",
+        lastname: "",
+        email: "",
+        image: null,
+        password: "",
+      });
+      if (imageRef.current) {
+        imageRef.current.value = "";
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const labelButton = isCreate ? "Create" : `Edit`;
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <h2>{isCreate ? "Create user" : `Edit user (${id})`}</h2>
+        <div className="form__inputs">
+          <div className="form__input-group">
+            <label className="form__label" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name="name"
+              value={user.name}
+              placeholder="Name"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form__input-group">
+            <label className="form__label" htmlFor="lastname">
+              Last name
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name="lastname"
+              value={user.lastname}
+              placeholder="Last name"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form__input-group">
+            <label className="form__label" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="form__input"
+              type="text"
+              name="email"
+              value={user.email}
+              placeholder="Email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form__input-group">
+            <label className="form__label" htmlFor="image">
+              Image
+            </label>
+            <input
+              className="form__input"
+              type="file"
+              name="image"
+              ref={imageRef}
+              accept="image/*"
+              placeholder="Image"
+              onChange={handleImageChange}
+            />
+          </div>
+          <div className="form__input-group">
+            <label className="form__label" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="form__input"
+              type="password"
+              name="password"
+              value={user.password}
+              placeholder="Password"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <button disabled={isLoading} type="submit" className="btn-primary">
+          {isLoading ? (
+            <LoaderCircle className="loader" width={18} height={18} />
+          ) : (
+            labelButton
+          )}
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default UserForm;
